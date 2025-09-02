9001b5c8969d5dfd5d4b962357bdd17d2af3efbe
Nakul Kumar <nakulagrawal716@gmail.com>
2025-09-02 11:35:44 +0530

    Add files via upload

---
+++ pythonassign3.py
@@ -0,0 +1,9 @@
+def factorial(n) :
+    if n < 2 :
+        return (1)
+    else :
+        return (n * factorial(n - 1))
+    
+y = int(input("Enter a number :"))
+x = factorial(y) 
+print(f"The factorial of {y} is : {x}")   
